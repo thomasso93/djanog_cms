d83e1bbdbc5403ff78db8997ccad19be158df1f0
Tomasz Sordon <tomasz.sordon@capgemini.com>
2016-10-26 15:05:28 +0200

    fixed variable naming

--- gui/controller/taskController.js
+++ gui/controller/taskController.js
@@ -3,6 +3,8 @@ var cms = angular.module('cms', []);
 cms.controller('taskController', function($scope, $http) {
     $http.get('http://127.0.0.1:8000/api/v1/task/?format=json').
         then(function(response) {
-            $scope.greeting = response.data;
+            var responseData = response.data;
+            $scope.tasksFull = responseData;
+            $scope.tasks = responseData.objects;
         });
 });
